60d208980e9cd5b8907df31c4827821187a89888
Leon RO <leon.rodriguez.ore@gmail.com>
2020-12-23 14:18:55 -0300

    botones de Redes sociales habilitados (linkeIn) habilitar ingreso de msj permisos de internet biblioteca de iconos GLIDE incorporada Metodo View Binding activado

--- app/src/main/java/com/example/perfilapp/ContactActivity.java
+++ app/src/main/java/com/example/perfilapp/ContactActivity.java
@@ -2,13 +2,32 @@ package com.example.perfilapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.perfilapp.databinding.ActivityContactBinding;
 
 public class ContactActivity extends AppCompatActivity {
+    private ActivityContactBinding  mbinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        mbinding=ActivityContactBinding.inflate(getLayoutInflater());
+        setContentView(mbinding.getRoot());
+
+        mbinding.imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent Implicito a el navegador
+                String urls = "https://www.linkedin.com/in/leonardo-rodriguez-orellana-b6606a134/";
+                Intent mIntentLinkIN = new Intent(Intent.ACTION_VIEW);
+                mIntentLinkIN.setData(Uri.parse(urls));
+                startActivity(mIntentLinkIN);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/perfilapp/MainActivity.java
+++ app/src/main/java/com/example/perfilapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.perfilapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+       //Todo view binding y aplicar
+        ImageView imageView = findViewById(R.id.imageView2);
+        Glide.with(this).load("https://unsplash.com/photos/mBtk_VI7x54/download?force=true&w=640").centerCrop().into(imageView );
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast.makeText(MainActivity.this, ":v HOLA", Toast.LENGTH_SHORT).show();*/
+                //intent , cambio de actividad , se debe instanciar y luego iniciar
+                Intent mIntent = new Intent(MainActivity.this,ContactActivity.class);
+                startActivity(mIntent);
             }
         });
     }
